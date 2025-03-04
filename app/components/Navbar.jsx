26047c572a86610f70d95bb945b65a82c62d41cd
Your Name <you@example.com>
2025-03-04 17:27:48 +0600

    glassed

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -9,21 +9,24 @@ import Container1 from "./Container1";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  //   useEffect(() => {
-  //   const handleResize = () => {
-  //     if (window.innerWidth <= 1024) {
-  //       setIsOpen(false);
-  //     }
-  //   };
-
-  //   window.addEventListener("resize", handleResize);
-  //   return () => window.removeEventListener("resize", handleResize);
-  // }, []);
-
+  const [isScrolled, setIsScrolled] = useState(false);
+  const scrollhandler = () => {
+    if (window.scrollY > 100) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", scrollhandler);
+    return () => {
+      window.removeEventListener("scroll", scrollhandler);
+    };
+  }, []);
   return (
-    <nav className=" absolute top-0 w-full z-50">
-      <div className="container_cust hidden lg:block   md:max-w-[1474px]    md:justify-center  relative md:mx-auto">
-        <div className="bg_nav hidden md:flex absolute -z-10  it w-[200px]  md:w-auto ">
+    <nav className="fixed top-0 w-full z-50">
+      <div className="container_cust hidden lg:block md:max-w-[1474px] md:justify-center relative md:mx-auto">
+        <div className="bg_nav hidden md:flex absolute -z-10 it w-[200px] md:w-auto">
           <div className="left_para absolute md:block hidden">
             <div className="parallelogram_2 absolute -left-5 z-20 bg-white"></div>
             <div className="parallelogram_1 absolute -left-3 mt-[18px] z-20 bg-white"></div>
@@ -32,30 +35,30 @@ const Navbar = () => {
           <div className="main_nav w-[1450px] "></div>
           <div className="right_para absolute top-0 right-0 ">
             <div className="parallelogram_3 absolute mt-[18px] z-20 bg-white"></div>
-            <div className="parallelogram_4 absolute  z-20 bg-white"></div>
+            <div className="parallelogram_4 absolute z-20 bg-white"></div>
           </div>
         </div>
-        <div className="main  relative flex items-center  md:justify-normal ">
+        <div className="main relative flex items-center md:justify-normal">
           <div className="logo hidden md:block">
             <Link href="/">
               <Image
                 src={Union}
-                alt={Union}
+                alt="Union Logo"
                 width={0}
                 height={0}
-                className="block md:absolute md:left-20   md:top-3"
+                className="block md:absolute md:left-20 md:top-3"
               />
             </Link>
           </div>
 
-          <div className="hidden md:block overflow-y-hidden-hidden transition-all duration-300 hover:scale-[101%]  px-10 ml-96  py-7 group  relative  w-[600px] justify-between text-white font-ox text-[20px] font-semibold ">
-            <div className="left_para absolute top-0 left-0  transition-all duration-300">
-              <div className="parallelogram_2 absolute transition-all -left-5  z-20 bg-red-600 group-hover:bg-white"></div>
+          <div className="hidden md:block overflow-y-hidden-hidden transition-all duration-300 hover:scale-[101%] px-10 ml-96 py-7 group relative w-[600px] justify-between text-white font-ox text-[20px] font-semibold">
+            <div className="left_para absolute top-0 left-0 transition-all duration-300">
+              <div className="parallelogram_2 absolute transition-all -left-5 z-20 bg-red-600 group-hover:bg-white"></div>
               <div className="parallelogram_1 absolute transition-all -left-3 mt-[18px] z-20 bg-red-600 group-hover:bg-white"></div>
             </div>
 
-            <ul className="flex items-center justify-between ">
-              <li className="transition-all duration-300 hover:text-[#ff2221] ">
+            <ul className="flex items-center justify-between">
+              <li className="transition-all duration-300 hover:text-[#ff2221]">
                 <Link className="" href="/">
                   Home
                 </Link>
@@ -71,19 +74,18 @@ const Navbar = () => {
               </li>
             </ul>
 
-            <div className="transition-all duration-300 right_para absolute top-0 right-0 ">
+            <div className="transition-all duration-300 right_para absolute top-0 right-0">
               <div className="parallelogram_3 absolute mt-[18px] transition-all z-20 bg-red-600 group-hover:bg-white"></div>
-              <div className="parallelogram_4 absolute transition-all  z-20 bg-red-600 group-hover:bg-white"></div>
+              <div className="parallelogram_4 absolute transition-all z-20 bg-red-600 group-hover:bg-white"></div>
             </div>
           </div>
 
           <Link
             href={"/contact"}
-            className="devneed hidden md:block absolute  right-1/4 top-1/2 
-          -translate-y-[25px] "
+            className="devneed hidden md:block absolute right-1/4 top-1/2 -translate-y-[25px]"
           >
-            <div className="animated-border-box-glow "></div>
-            <div className=" px-4 font-bold text-white py-2 animated-border-box font-ox text-[20px]">
+            <div className="animated-border-box-glow"></div>
+            <div className="px-4 font-bold text-white py-2 animated-border-box font-ox text-[20px]">
               Dev Needed? 🖥️
             </div>
           </Link>
@@ -92,40 +94,44 @@ const Navbar = () => {
 
       {/* responsive */}
 
-      <Container1 className={`menu_mobile lg:hidden flex  flex-col !px-0    `}>
+      <Container1
+        className={`${
+          isScrolled ? "glass" : "bg-transparent"
+        } menu_mobile lg:hidden flex flex-col !px-0`}
+      >
         <div
           className={`flex-1 flex px-6 py-5 pb-3 items-center justify-between  ${
             isOpen ? "bg-[#FCF7F8]" : "bg-transparent"
-          } `}
+          }`}
         >
           <Link
             href="/"
-            className={`font-pira  text-4xl transition-all duration-500  ${
-              isOpen ? " text-[#ff2221]" : "text-[#FCF7F8]"
+            className={`font-pira text-4xl transition-all duration-500 ${
+              isOpen ? "text-[#ff2221]" : "text-[#FCF7F8]"
             }`}
           >
             RASHA
           </Link>
           <div
             className={`transition-all duration-500 p-1 rounded-md ${
-              isOpen ? "bg-[#FCF7F8] " : "bg-[#ff2221]"
-            } `}
+              isOpen ? "bg-[#FCF7F8]" : "bg-[#ff2221]"
+            }`}
             onClick={() => setIsOpen(!isOpen)}
           >
             {isOpen ? (
-              <X className="size-7  text-[#ff2221]" />
+              <X className="size-7 text-[#ff2221]" />
             ) : (
-              <Menu className="size-7  text-[#FCF7F8]" />
+              <Menu className="size-7 text-[#FCF7F8]" />
             )}
           </div>
         </div>
 
         <div
-          className={`bg-[#FCF7F8] flex-1    ${
+          className={`bg-[#FCF7F8] flex-1 ${
             isOpen ? "block border-t-2 border-[#ff2221]" : "hidden"
           }`}
         >
-          <ul className="flex flex-col   text-[#ff2221] font-ox  font-semibold">
+          <ul className="flex flex-col text-[#ff2221] font-ox font-semibold">
             <Link
               href={"/"}
               className="transition-all ulslide px-6 py-3 duration-300 hover:text-[#ff2221] border-b-2 border-[#ff2221]"
@@ -153,8 +159,6 @@ const Navbar = () => {
           </ul>
         </div>
       </Container1>
-
-      {/* responsive */}
     </nav>
   );
 };
