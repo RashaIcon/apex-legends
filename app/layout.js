c7f849e61a1d849b9dc0b9f3f61ab2086edf2b1d
Your Name <you@example.com>
2025-03-04 14:32:03 +0600

    popasd

--- app/layout.js
+++ app/layout.js
@@ -4,15 +4,23 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
+import icon from "./favicon.ico";
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
+      <head>
+        
+        <title>Rasha</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>
         <Navbar />
         {children}
         <Toaster />
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
